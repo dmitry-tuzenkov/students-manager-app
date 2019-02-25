154800a7ada9f942e6a4fb4ea31053c01bc07770
Dmitry Tuzenkov <dmitry.tuzenkov@gmail.com>
2019-02-26 01:05:45 +0200

    Updates

---
+++ students-manager-app/precache-manifest.6eaf308dc85ffb2e889f2699b74e0da7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1c9b5eb52a88840e6a37",
+    "url": "/static/js/main.1c9b5eb5.chunk.js"
+  },
+  {
+    "revision": "cdd8683658e3a74dccfd",
+    "url": "/static/js/2.cdd86836.chunk.js"
+  },
+  {
+    "revision": "1c9b5eb52a88840e6a37",
+    "url": "/static/css/main.5af88a1b.chunk.css"
+  },
+  {
+    "revision": "1073c5c5a63107a1595a0ad4e6380f1d",
+    "url": "/index.html"
+  }
+];
